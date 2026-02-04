c8a5806c8367f7eee60d00d36d56bf2cfb64da6c
Boban Blagojevic <boban.blagojevic@tsp.edu.rs>
2026-02-04 23:56:41 +0100

    Add Moodle 5.x support
    - Update version.php with supported range [404, 510]
    - Update CI to test Moodle 4.4, 5.0, and main branch
    - Bump version to 0.1.1

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gear';
-$plugin->version = 2026020400;
-$plugin->requires = 2024042200; // Moodle 4.4+.
+$plugin->version = 2026020401;
+$plugin->requires = 2024042200; // Moodle 4.4+ (also compatible with 5.x).
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.0';
+$plugin->release = '0.1.1';
+$plugin->supported = [404, 510]; // Moodle 4.4 to 5.1.
