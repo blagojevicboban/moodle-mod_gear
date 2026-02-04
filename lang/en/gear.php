1024a323b126648fd6d8ec946b27b023e3bc3294
Boban Blagojevic <boban.blagojevic@tsp.edu.rs>
2026-02-04 23:50:21 +0100

    Fix CI lint errors
    - Reorder language strings alphabetically (en, sr)
    - Remove section comments from lang files
    - Remove blank line after opening brace in classes
    - Fix class formatting per Moodle PHPCS rules

--- lang/en/gear.php
+++ lang/en/gear.php
@@ -22,68 +22,52 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'GEAR - Immersive Learning';
-$string['modulename'] = 'GEAR';
-$string['modulenameplural'] = 'GEAR Activities';
-$string['modulename_help'] = 'The GEAR activity module enables teachers to create immersive AR/VR learning experiences with 3D models, hotspots, and interactive elements.';
-
-// Capabilities.
-$string['gear:addinstance'] = 'Add a new GEAR activity';
-$string['gear:view'] = 'View GEAR content';
-$string['gear:manage'] = 'Manage GEAR content';
-
-// Form fields.
-$string['gearname'] = 'Activity name';
-$string['gearname_help'] = 'Enter a name for this AR/VR activity.';
-$string['geardescription'] = 'Description';
-
-// Scene settings.
-$string['scenesettings'] = 'Scene Settings';
+$string['addhotspot'] = 'Add hotspot';
+$string['addmodel'] = 'Add 3D model';
 $string['ar_enabled'] = 'Enable AR mode';
 $string['ar_enabled_help'] = 'Allow users to view content in Augmented Reality on supported devices.';
-$string['vr_enabled'] = 'Enable VR mode';
-$string['vr_enabled_help'] = 'Allow users to view content in Virtual Reality with a headset.';
+$string['arnotsupported'] = 'AR is not supported on this device.';
+$string['autorotate'] = 'Auto-rotate';
 $string['background_color'] = 'Background color';
-$string['lighting'] = 'Lighting preset';
-$string['lighting_studio'] = 'Studio';
-$string['lighting_outdoor'] = 'Outdoor';
-$string['lighting_dark'] = 'Dark';
-
-// Models.
-$string['models'] = '3D Models';
-$string['addmodel'] = 'Add 3D model';
-$string['modelfile'] = 'Model file';
-$string['modelfile_help'] = 'Upload a 3D model in glTF (.gltf, .glb) format.';
-$string['supportedformats'] = 'Supported formats: glTF, GLB';
-
-// Hotspots.
-$string['hotspots'] = 'Hotspots';
-$string['addhotspot'] = 'Add hotspot';
-$string['hotspottype'] = 'Hotspot type';
-$string['hotspottype_info'] = 'Information';
-$string['hotspottype_quiz'] = 'Quiz';
-$string['hotspottype_audio'] = 'Audio';
-$string['hotspottype_link'] = 'Link';
-$string['hotspottitle'] = 'Hotspot title';
-$string['hotspotcontent'] = 'Hotspot content';
-
-// Viewer.
-$string['enterarvr'] = 'Enter AR/VR';
 $string['enterar'] = 'View in AR';
+$string['enterarvr'] = 'Enter AR/VR';
 $string['entervr'] = 'Enter VR';
 $string['exitvr'] = 'Exit VR';
 $string['fullscreen'] = 'Fullscreen';
-$string['autorotate'] = 'Auto-rotate';
-$string['loadingmodel'] = 'Loading 3D model...';
-
-// Errors.
-$string['nomodels'] = 'No 3D models have been added to this activity.';
-$string['webxrnotsupported'] = 'WebXR is not supported in your browser.';
-$string['arnotsupported'] = 'AR is not supported on this device.';
+$string['gear:addinstance'] = 'Add a new GEAR activity';
+$string['gear:manage'] = 'Manage GEAR content';
+$string['gear:view'] = 'View GEAR content';
+$string['geardescription'] = 'Description';
+$string['gearname'] = 'Activity name';
+$string['gearname_help'] = 'Enter a name for this AR/VR activity.';
+$string['hotspotcontent'] = 'Hotspot content';
+$string['hotspots'] = 'Hotspots';
+$string['hotspottitle'] = 'Hotspot title';
+$string['hotspottype'] = 'Hotspot type';
+$string['hotspottype_audio'] = 'Audio';
+$string['hotspottype_info'] = 'Information';
+$string['hotspottype_link'] = 'Link';
+$string['hotspottype_quiz'] = 'Quiz';
 $string['invalidcolor'] = 'Invalid color format. Please use #RRGGBB format.';
-
-// Privacy.
+$string['lighting'] = 'Lighting preset';
+$string['lighting_dark'] = 'Dark';
+$string['lighting_outdoor'] = 'Outdoor';
+$string['lighting_studio'] = 'Studio';
+$string['loadingmodel'] = 'Loading 3D model...';
+$string['modelfile'] = 'Model file';
+$string['modelfile_help'] = 'Upload a 3D model in glTF (.gltf, .glb) format.';
+$string['models'] = '3D Models';
+$string['modulename'] = 'GEAR';
+$string['modulename_help'] = 'The GEAR activity module enables teachers to create immersive AR/VR learning experiences with 3D models, hotspots, and interactive elements.';
+$string['modulenameplural'] = 'GEAR Activities';
+$string['nomodels'] = 'No 3D models have been added to this activity.';
+$string['pluginname'] = 'GEAR - Immersive Learning';
 $string['privacy:metadata:gear_tracking'] = 'Information about user interactions with GEAR content.';
-$string['privacy:metadata:gear_tracking:userid'] = 'The ID of the user.';
 $string['privacy:metadata:gear_tracking:action'] = 'The action performed by the user.';
 $string['privacy:metadata:gear_tracking:timecreated'] = 'The time when the action was recorded.';
+$string['privacy:metadata:gear_tracking:userid'] = 'The ID of the user.';
+$string['scenesettings'] = 'Scene Settings';
+$string['supportedformats'] = 'Supported formats: glTF, GLB';
+$string['vr_enabled'] = 'Enable VR mode';
+$string['vr_enabled_help'] = 'Allow users to view content in Virtual Reality with a headset.';
+$string['webxrnotsupported'] = 'WebXR is not supported in your browser.';
--- lang/sr/gear.php
+++ lang/sr/gear.php
@@ -22,68 +22,52 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'GEAR - Imerzivno učenje';
-$string['modulename'] = 'GEAR';
-$string['modulenameplural'] = 'GEAR Aktivnosti';
-$string['modulename_help'] = 'GEAR modul omogućava nastavnicima kreiranje imerzivnih AR/VR obrazovnih iskustava sa 3D modelima, hotspotovima i interaktivnim elementima.';
-
-// Capabilities.
-$string['gear:addinstance'] = 'Dodaj novu GEAR aktivnost';
-$string['gear:view'] = 'Pregledaj GEAR sadržaj';
-$string['gear:manage'] = 'Upravljaj GEAR sadržajem';
-
-// Form fields.
-$string['gearname'] = 'Naziv aktivnosti';
-$string['gearname_help'] = 'Unesite naziv za ovu AR/VR aktivnost.';
-$string['geardescription'] = 'Opis';
-
-// Scene settings.
-$string['scenesettings'] = 'Podešavanja scene';
+$string['addhotspot'] = 'Dodaj hotspot';
+$string['addmodel'] = 'Dodaj 3D model';
 $string['ar_enabled'] = 'Omogući AR režim';
 $string['ar_enabled_help'] = 'Dozvoli korisnicima da pregledaju sadržaj u proširenoj stvarnosti na podržanim uređajima.';
-$string['vr_enabled'] = 'Omogući VR režim';
-$string['vr_enabled_help'] = 'Dozvoli korisnicima da pregledaju sadržaj u virtuelnoj stvarnosti sa headsetom.';
+$string['arnotsupported'] = 'AR nije podržan na ovom uređaju.';
+$string['autorotate'] = 'Auto-rotacija';
 $string['background_color'] = 'Boja pozadine';
-$string['lighting'] = 'Preset osvetljenja';
-$string['lighting_studio'] = 'Studio';
-$string['lighting_outdoor'] = 'Spoljašnje';
-$string['lighting_dark'] = 'Tamno';
-
-// Models.
-$string['models'] = '3D Modeli';
-$string['addmodel'] = 'Dodaj 3D model';
-$string['modelfile'] = 'Fajl modela';
-$string['modelfile_help'] = 'Učitajte 3D model u glTF (.gltf, .glb) formatu.';
-$string['supportedformats'] = 'Podržani formati: glTF, GLB';
-
-// Hotspots.
-$string['hotspots'] = 'Hotspotovi';
-$string['addhotspot'] = 'Dodaj hotspot';
-$string['hotspottype'] = 'Tip hotspota';
-$string['hotspottype_info'] = 'Informacija';
-$string['hotspottype_quiz'] = 'Kviz';
-$string['hotspottype_audio'] = 'Audio';
-$string['hotspottype_link'] = 'Link';
-$string['hotspottitle'] = 'Naslov hotspota';
-$string['hotspotcontent'] = 'Sadržaj hotspota';
-
-// Viewer.
-$string['enterarvr'] = 'Uđi u AR/VR';
 $string['enterar'] = 'Pregledaj u AR';
+$string['enterarvr'] = 'Uđi u AR/VR';
 $string['entervr'] = 'Uđi u VR';
 $string['exitvr'] = 'Izađi iz VR';
 $string['fullscreen'] = 'Ceo ekran';
-$string['autorotate'] = 'Auto-rotacija';
-$string['loadingmodel'] = 'Učitavanje 3D modela...';
-
-// Errors.
-$string['nomodels'] = 'Nema dodatih 3D modela u ovoj aktivnosti.';
-$string['webxrnotsupported'] = 'WebXR nije podržan u vašem pregledaču.';
-$string['arnotsupported'] = 'AR nije podržan na ovom uređaju.';
+$string['gear:addinstance'] = 'Dodaj novu GEAR aktivnost';
+$string['gear:manage'] = 'Upravljaj GEAR sadržajem';
+$string['gear:view'] = 'Pregledaj GEAR sadržaj';
+$string['geardescription'] = 'Opis';
+$string['gearname'] = 'Naziv aktivnosti';
+$string['gearname_help'] = 'Unesite naziv za ovu AR/VR aktivnost.';
+$string['hotspotcontent'] = 'Sadržaj hotspota';
+$string['hotspots'] = 'Hotspotovi';
+$string['hotspottitle'] = 'Naslov hotspota';
+$string['hotspottype'] = 'Tip hotspota';
+$string['hotspottype_audio'] = 'Audio';
+$string['hotspottype_info'] = 'Informacija';
+$string['hotspottype_link'] = 'Link';
+$string['hotspottype_quiz'] = 'Kviz';
 $string['invalidcolor'] = 'Neispravan format boje. Koristite format #RRGGBB.';
-
-// Privacy.
+$string['lighting'] = 'Preset osvetljenja';
+$string['lighting_dark'] = 'Tamno';
+$string['lighting_outdoor'] = 'Spoljašnje';
+$string['lighting_studio'] = 'Studio';
+$string['loadingmodel'] = 'Učitavanje 3D modela...';
+$string['modelfile'] = 'Fajl modela';
+$string['modelfile_help'] = 'Učitajte 3D model u glTF (.gltf, .glb) formatu.';
+$string['models'] = '3D Modeli';
+$string['modulename'] = 'GEAR';
+$string['modulename_help'] = 'GEAR modul omogućava nastavnicima kreiranje imerzivnih AR/VR obrazovnih iskustava sa 3D modelima, hotspotovima i interaktivnim elementima.';
+$string['modulenameplural'] = 'GEAR Aktivnosti';
+$string['nomodels'] = 'Nema dodatih 3D modela u ovoj aktivnosti.';
+$string['pluginname'] = 'GEAR - Imerzivno učenje';
 $string['privacy:metadata:gear_tracking'] = 'Informacije o interakcijama korisnika sa GEAR sadržajem.';
-$string['privacy:metadata:gear_tracking:userid'] = 'ID korisnika.';
 $string['privacy:metadata:gear_tracking:action'] = 'Akcija koju je korisnik izvršio.';
 $string['privacy:metadata:gear_tracking:timecreated'] = 'Vreme kada je akcija zabeležena.';
+$string['privacy:metadata:gear_tracking:userid'] = 'ID korisnika.';
+$string['scenesettings'] = 'Podešavanja scene';
+$string['supportedformats'] = 'Podržani formati: glTF, GLB';
+$string['vr_enabled'] = 'Omogući VR režim';
+$string['vr_enabled_help'] = 'Dozvoli korisnicima da pregledaju sadržaj u virtuelnoj stvarnosti sa headsetom.';
+$string['webxrnotsupported'] = 'WebXR nije podržan u vašem pregledaču.';
