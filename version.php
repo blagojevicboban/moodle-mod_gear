c4db1a8b7cf815823ef21ffc07347cc7dc04792b
Boban Blagojevic <boban.blagojevic@tsp.edu.rs>
2026-03-11 07:23:42 +0100

    Add Moodle 4.1 and 4.2 to CI matrix and update version requirements

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_gear';
 $plugin->version = 2026031103;
-$plugin->requires = 2024042200; // Moodle 4.4+ (also compatible with 5.x).
+$plugin->requires = 2022112800; // Moodle 4.1+ (also compatible with 5.x).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
-$plugin->supported = [404, 510]; // Moodle 4.4 to 5.1.
+$plugin->supported = [401, 510]; // Moodle 4.1 to 5.1.
